15f27e1e0ec2501250376e1888562e50f7074623
Joanna Rose Tandang <99891655+nyoannarose@users.noreply.github.com>
2022-03-04 11:18:48 +0800

    Add files via upload

---
+++ ch3_ex1.c
@@ -0,0 +1,16 @@
+//Variable Declaration
+int i, j;
+float x, y;
+
+//Assigning Values
+i = 10;
+j = 20;
+x = 43.2892f;
+y = 5527.0f;
+
+//Printing the Results
+printf("i = %d, j = %d,, x = %f; y = %f\n", i, j, x, y); 
+
+/*%d- decimal integer
+%f- floating point number*/
+
