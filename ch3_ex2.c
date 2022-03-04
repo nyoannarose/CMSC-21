3d130fa92abb4506cabcfc1c6aa39e69c1958e90
Joanna Rose Tandang <99891655+nyoannarose@users.noreply.github.com>
2022-03-04 10:41:31 +0800

    Add files via upload

---
+++ ch3_ex2.c
@@ -0,0 +1,20 @@
+//C Program that prints int and float values in various formats
+
+#include <stdio.h> //Preprocessor Directive
+
+int main (void) //Main Function 
+{
+    //Data Types
+    int i; 
+    float x; 
+
+    //Assigning Values
+    i = 40;
+    x = 839.21f;
+    
+    //Printing the Results
+    printf("|%d|%5d|%-5d|%5.3d|\n", i, i, i, i) ; //Display integers in various formats
+    printf("|%10.3f|%10.3e|%-10g|\n", x, x, x) ; //Display floats in various formats
+    
+    return 0;
+}
