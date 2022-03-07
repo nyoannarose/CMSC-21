200b69b784aeb5da2fd8593cead135439151a4a2
Joanna Rose Tandang <99891655+nyoannarose@users.noreply.github.com>
2022-03-07 21:26:20 +0800

    Add files via upload

---
+++ Lecture 1 Assignment/assignment_lec1_c2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    int num1=87,num2=15;//variable declaration
+    int sub=num1-num2;//subtraction
+    printf("The difference of %d and %d is %d\n",num1,num2,sub);
+    return 0;
+}
---
+++ Lecture 1 Assignment/assignment_lec1_c4.c
@@ -0,0 +1,10 @@
+#include <stdio.h> 
+int main(void){
+
+//COMPUTE RESULT 
+int sum = 25 + 37 + 19;
+
+//DISPLAY RESULTS 
+printf ("The answer is %d\n", sum); 
+return 0;
+}
