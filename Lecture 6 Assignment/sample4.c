1f62c19015103468864c8cad501700f6b11e9f22
Joanna Rose Tandang <99891655+nyoannarose@users.noreply.github.com>
2022-04-29 10:19:55 +0800

    Add files via upload

---
+++ Lecture 6 Assignment/sample4.c
@@ -0,0 +1,24 @@
+/* Reverses a series of numbers */
+#include <stdio.h>
+#define n_students 5
+
+int main(void){
+	float grades[n_students], average, sum;
+	int i;
+
+	
+	for (i=0; i<n_students; i++){
+		printf("Enter grade for student %d: ", i+1);
+		scanf("%f", &grades[i]);
+	}
+
+	for(i=0; i<n_students; i++)
+	{
+		sum += grades[i];
+	}
+
+	average=sum/n_students;
+	printf("%.2f \n", average);
+
+	return 0;
+}
